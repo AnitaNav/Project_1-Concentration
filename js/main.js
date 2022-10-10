a30690d3d8598d571743699e00b78ff0a5da5e59
AnitaNav <anitakm215@gmail.com>
2022-10-09 22:47:23 -0700

    Change html, css and JS

--- js/main.js
+++ js/main.js
@@ -1,38 +1,63 @@
 /*----- constants -----*/
-const CHAR_LOOKUP = {
-       kai : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyeJCrpSbgrh859l_lmJYrdEPZr2QMYfXnGQ&usqp=CAU',
-       lloyd : 'https://www.kindpng.com/picc/m/483-4832798_lloyd-ninjago-lego-ninjago-lloyd-hd-png-download.png',
-       jay : 'https://www.nicepng.com/png/detail/178-1788079_related-wallpapers-lego-dimensions-1-fun-ninjago-jay.png',
-       wu :'https://i.pinimg.com/564x/19/2d/5c/192d5c7931b4f4a0116d5a2ca41c0379.jpg',
-       cole: 'https://images-na.ssl-images-amazon.com/images/I/51oEznB3BSL._AC_UL600_SR600,600_.jpg',
-       zane: 'https://i.etsystatic.com/9915541/r/il/3ae1ee/3477277820/il_570xN.3477277820_a9m3.jpg'
-};
+const CHAR_LOOKUP = [
+       {img : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyeJCrpSbgrh859l_lmJYrdEPZr2QMYfXnGQ&usqp=CAU', matched : false},
+       {img : 'https://www.kindpng.com/picc/m/483-4832798_lloyd-ninjago-lego-ninjago-lloyd-hd-png-download.png', matched : false},
+       {img : 'https://www.nicepng.com/png/detail/178-1788079_related-wallpapers-lego-dimensions-1-fun-ninjago-jay.png', matched : false},
+       {img :'https://i.pinimg.com/564x/19/2d/5c/192d5c7931b4f4a0116d5a2ca41c0379.jpg', matched : false},
+       {img: 'https://images-na.ssl-images-amazon.com/images/I/51oEznB3BSL._AC_UL600_SR600,600_.jpg', matched : false},
+       {img: 'https://i.etsystatic.com/9915541/r/il/3ae1ee/3477277820/il_570xN.3477277820_a9m3.jpg', matched : false}
+];
+const CARD_BACK = 'https://images.squarespace-cdn.com/content/v1/5bbe8cab7eb88c0882112cab/1640105542555-5RXB0HGSIOFK7KK3A9HJ/y7878yuuyuy.png?format=500w';
 
   /*----- state variables -----*/
-let board;
-let card1;
-let card2;
-let matched = 0;
-let chances = 8;
+let cardS; // Array of 12 cards
+let firstCoice; // First card clicked
+//let maxGuess= 8;
 
   /*----- cached elements  -----*/
-const cards = document.getElementsById('memory-card');
-const playButton = document.querySelector('button');
+// const cards = document.getElementsById('memory-card');
+// const playButton = document.querySelector('button');
 
   /*----- event listeners -----*/
-  document.getElementById('memory-game').addEventListener('click', handleMove);
-  playButton.addEventListener('click', initialize);
+  // document.getElementById('memory-game').addEventListener('click', handleMove);
+  //playButton.addEventListener('click', initialize);
 
   /*----- functions -----*/
   initialize();
-
+// board should be intialized to start the game
+//player will be allowed to select two cards from the board
+// player should not be allowed to open more than two cards
+//cards are compared -> if matched cards stay open else flip back
+//the selected pair of cards should be stored temporarily in an element
+// board should be rendered back if cards are unmatched
+//if all cards are matched -> game over
+//if player runs out of chances -> game over
   function initialize(){
+    cards = shuffle();
+
 
 
     render();
 
   }
+// Shuffle cards 
+  function shuffle() {
+let tempCards = [];
+let cards = []; // Shuffled cards array
 
-  function render(){
+for (let card of CHAR_LOOKUP) {
+  tempCards.push(card, card);
+}
 
+//console.log(tempCards);
+while (tempCards.length>0) {
+  let rndIdx = Math.floor(Math.random() * tempCards.length);
+  let card = tempCards.splice(rndIdx, 1)[0];
+  cards.push(card);
+  //console.log(card);
   }
+
+  return cards;
+
+}
+
--- planning/pseudocode.md
+++ planning/pseudocode.md
@@ -3,6 +3,14 @@ Pseudocode :
 If the flipped pair of cards match , then it is a win for the player and the cards remain open.
 If the cards do not match, the selected pair of cards remain open for a few seconds and  flip back.
 The player would get eight chances to win the game (pick all the pairs).
+// board should be intialized to start the game
+//player will be allowed to select two cards from the board
+// player should not be allowed to open more than two cards
+//cards are compared -> if matched cards stay open else flip back
+//the selected pair of cards should be stored temporarily in an element
+// board should be rendered back if cards are unmatched
+//if all cards are matched -> game over
+//if player runs out of chances -> game over
 
 
 Constants : Player, Cards, Turns
